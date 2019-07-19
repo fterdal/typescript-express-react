395a9fb2e4720871af252e8600e0100896c3201e
Finn <finnterdal@gmail.com>
2019-07-19 11:16:39 -0500

    install and create webpack and babel things

---
+++ babel.config.js
@@ -0,0 +1,3 @@
+export default {
+  presets: ['@babel/react', '@babel/env'],
+}
---
+++ src/client/index.ts
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return (
+    <div>Hello from React and Typescript!</div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
