685767525a76ca772f7155715193a6bef9fd264b
Finn <finnterdal@gmail.com>
2019-07-19 12:08:20 -0500

    install milligram and add a navabr

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const NavBar = ({ children }: { children: any }) => {
+  return <nav>{children}</nav>
+}
+
 const App = () => {
   return (
-    <div>Hello from React and Typescript!</div>
+    <>
+      <NavBar>
+        <h1>Hello Typescript</h1>
+      </NavBar>
+      <ul>
+        <li>
+          <a href="/api/cookies">All Cookies</a>
+        </li>
+        <li>
+          <a href="/api/cookies/1">Cookie: 1</a>
+        </li>
+      </ul>
+    </>
   )
 }
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-)
+ReactDOM.render(<App />, document.getElementById('app'))
