925e2799978002e3403a7306a765003a93ff0ea5
Finn <finnterdal@gmail.com>
2019-07-22 00:22:21 -0500

    messing with react hooks and such

--- src/client/components/SingleCookie.tsx
+++ src/client/components/SingleCookie.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 
 import { AppState, Cookie } from '../store'
@@ -9,12 +9,21 @@ export const SingleCookie = (props: any) => {
   const selectedCookie = useSelector((store: AppState) =>
     store.cookies.find(cookie => cookie.id === selectedId)
   )
+
+  // This early return before the hooks will cause React a lot of confusion!
+  // Gonna have to refactor...
   if (!selectedCookie) return <div>Couldn't find this cookie!</div>
   const { name, quantity, glutenFree } = selectedCookie
+  const [newName, setNewName] = useState(name)
+  const [newQuantity, setNewQuantity] = useState(quantity)
+  const [newGlutenFree, setNewGlutenFree] = useState(glutenFree)
+  console.log(newName)
   return (
     <div className="single-cookie">
       <div>
-        <h2>{name}</h2>
+        <h2>
+          <input value={newName} onChange={evt => setNewName(evt.target.value)} />
+        </h2>
         <div>Quantity: {quantity}</div>
         <div>{glutenFree ? 'Gluten Free!' : 'Contains Gluten'}</div>
       </div>
