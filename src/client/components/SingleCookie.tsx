822683f5c993576ad48ca72b3e839e036097e408
Finn <finnterdal@gmail.com>
2019-07-23 12:48:39 -0500

    can now delete cookies

--- src/client/components/SingleCookie.tsx
+++ src/client/components/SingleCookie.tsx
@@ -1,7 +1,11 @@
 import React, { useState, FormEvent } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
-import { AppState, Cookie, putCookie } from '../store'
+import { AppState, Cookie, putCookie, deleteCookie } from '../store'
+
+// We need this wrapper/helper pattern here. If we tried to write an early return
+// as above, we couldn't also use hooks with the resulting data, because they may
+// not fire in the same order on successive renders.
 
 import './SingleCookie.scss'
 export const SingleCookie = (props: any) => {
@@ -9,17 +13,28 @@ export const SingleCookie = (props: any) => {
   const selectedCookie = useSelector((store: AppState) =>
     store.cookies.find(cookie => cookie.id === selectedId)
   )
+  const redirectToCookies = () => {
+    props.history.push('/cookies')
+  }
   if (!selectedCookie) {
     return <div className="cookie-not-found">Cookie Not Found</div>
   }
-  return <SingleCookieHelper {...selectedCookie} />
+  return (
+    <SingleCookieHelper
+      {...selectedCookie}
+      id={selectedId}
+      redirect={redirectToCookies}
+    />
+  )
 }
 
-// We need this wrapper/helper pattern here. If we tried to write an early return
-// as above, we couldn't also use hooks with the resulting data, because they may
-// not fire in the same order on successive renders.
-const SingleCookieHelper = (props: Cookie) => {
-  const { id, name, quantity, glutenFree } = props
+interface SingleCookieProps extends Cookie {
+  id: number
+  redirect: () => void
+}
+
+const SingleCookieHelper = (props: SingleCookieProps) => {
+  const { id, name, quantity, glutenFree, redirect } = props
   const [newName, setNewName] = useState(name)
   const [newQuantity, setNewQuantity] = useState(quantity)
   const [newGlutenFree, setNewGlutenFree] = useState(glutenFree)
@@ -27,9 +42,8 @@ const SingleCookieHelper = (props: Cookie) => {
 
   const handleSubmit = (evt: FormEvent) => {
     evt.preventDefault()
-    console.log({ newName, newQuantity, newGlutenFree })
     dispatch(
-      putCookie(id as number, {
+      putCookie(id, {
         name: newName,
         quantity: newQuantity,
         glutenFree: newGlutenFree,
@@ -37,9 +51,9 @@ const SingleCookieHelper = (props: Cookie) => {
     )
   }
 
-  const handleDelete = (evt: /* MouseEvent */ any) => {
-    console.log(evt.target)
-    // dispatch(deleteCookie(id))
+  const handleDelete = () => {
+    dispatch(deleteCookie(id))
+    redirect()
   }
 
   return (
